cf297871f54badb1dc2cccd9f6915cf737143803
itsprk <prk.shine@gmail.com>
2019-10-21 23:06:43 +0530

    Updates

---
+++ precache-manifest.b73d81d86a2d50f66832902652f4cd48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "032f6a4e18fe89eff2eba2507ef35b11",
+    "url": "/RecipeSearch/index.html"
+  },
+  {
+    "revision": "390b36fd1acd6efc8bd5",
+    "url": "/RecipeSearch/static/css/main.67f3b7c4.chunk.css"
+  },
+  {
+    "revision": "07125c670c6ec90ba4d7",
+    "url": "/RecipeSearch/static/js/2.72ab4ca7.chunk.js"
+  },
+  {
+    "revision": "390b36fd1acd6efc8bd5",
+    "url": "/RecipeSearch/static/js/main.8c3f6036.chunk.js"
+  },
+  {
+    "revision": "c6b4732d1a751594563a",
+    "url": "/RecipeSearch/static/js/runtime-main.3d60b482.js"
+  }
+]);
